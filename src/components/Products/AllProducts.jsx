052f6e7fcfde07f0d1b7cb39e471cc87853cb209
Rohan Joby <rohanjobyak@gmail.com>
2021-12-04 17:45:56 +0530

    fix: add error message

--- src/components/Products/AllProducts.jsx
+++ src/components/Products/AllProducts.jsx
@@ -8,11 +8,21 @@ import classes from "./ProductItem.module.css";
 const AllProducts = () => {
   const { filtered_products } = useFilters();
 
-  let products = filtered_products.map((prdt) => (
-    <ProductItem key={prdt.id} data={prdt} />
-  ));
-
-  return <div className={classes.grid}>{products}</div>;
+  let products;
+  if (filtered_products && filtered_products.length > 0) {
+    products = filtered_products.map((prdt) => (
+      <ProductItem key={prdt.id} data={prdt} />
+    ));
+    return <div className={classes.grid}>{products}</div>;
+  } else {
+    products = (
+      <>
+        <h2>Sorry, No products match the search parameters</h2>
+        <h3>Please modify the filtes!</h3>
+      </>
+    );
+    return <div className={classes.empty}>{products}</div>;
+  }
 };
 
 export default AllProducts;
